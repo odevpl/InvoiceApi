3766edb6482ee8ba850c48742b3e7e01a923ae7e
Marcin Sachs <marcinsachs@gmail.com>
2026-01-11 21:45:36 +0100

    Add environment variable support and configuration files for JWT authentication

--- api/config/settings.py
+++ api/config/settings.py
@@ -1,3 +1,8 @@
-SECRET_KEY = "f61788f818d27b0f9edfcb29645068ab5589f867d61f1f6bba4b0b9f3bbe6fe8"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 15
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))

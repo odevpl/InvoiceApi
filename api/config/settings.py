ef4784707281e6188cce76479cce038f0c873956
Adam_Gadaczek <adam.gadaczek@wp.pl>
2026-01-14 16:01:11 +0100

    DB configuration

--- api/config/settings.py
+++ api/config/settings.py
@@ -1,8 +1,10 @@
 from pydantic_settings import BaseSettings
+from pydantic import computed_field
 
 class Settings(BaseSettings):
     # JWT
     SECRET_KEY: str
+    ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
@@ -12,6 +14,13 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+
+    @computed_field 
+    def DATABASE_URL(self) -> str: 
+        return ( 
+            f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}" 
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}" 
+        )
 
     class Config:
         env_file = ".env"

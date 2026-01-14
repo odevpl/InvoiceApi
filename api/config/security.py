4a1057df580d1dfd114293781962c3215261ef7d
Adam_Gadaczek <adam.gadaczek@wp.pl>
2026-01-14 17:34:22 +0100

    Change in security.py and settings.py

--- api/config/security.py
+++ api/config/security.py
@@ -1,9 +1,6 @@
 from api.config.settings import settings
 from fastapi.security import OAuth2PasswordBearer
 
-SECRET_KEY = settings.SECRET_KEY
-ALGORITHM = settings.ALGORITHM
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
-REFRESH_TOKEN_EXPIRE_DAYS = settings.REFRESH_TOKEN_EXPIRE_DAYS
+ALGORITHM = "HS256"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
--- api/config/settings.py
+++ api/config/settings.py
@@ -4,7 +4,6 @@ from pydantic import computed_field
 class Settings(BaseSettings):
     # JWT
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 

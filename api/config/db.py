5c3fc0701297b5fb1fb02c5369c985bb3a6c8635
Adam_Gadaczek <adam.gadaczek@wp.pl>
2026-01-14 17:57:53 +0100

    Change in db.py

--- api/config/db.py
+++ api/config/db.py
@@ -3,13 +3,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from collections.abc import Generator 
 from api.config.settings import settings
 
-
-DATABASE_URL = (
-    f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}"
-    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-)
-
-engine = create_engine(DATABASE_URL, echo=False, future=True)
+engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,

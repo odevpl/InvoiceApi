90ed9ac17e99a5b3815ffbcc995ee4eec39c0931
Adam_Gadaczek <adam.gadaczek@wp.pl>
2026-01-25 10:49:07 +0100

    Refactor routes/clients.py

--- api/routes/clients.py
+++ api/routes/clients.py
@@ -1,4 +1,7 @@
 from fastapi import Depends, APIRouter, status, HTTPException
+from sqlalchemy.orm import Session
+
+from api.config.db import get_db
 from api.models.user import User
 from api.services.auth_service import get_current_active_user
 from api.services.client_service import add_client
@@ -28,10 +31,11 @@ Create a new client.
 """)
 async def create_client(
     client: ClientCreate,
+    db: Session = Depends(get_db),
     current_user: User = Depends(get_current_active_user),
 ):
     try:
-        db_client = add_client(client, owner_id=current_user.id)
+        db_client = add_client(db, client, owner_id=current_user.id)
     except ValueError as e:
         raise HTTPException(status_code=409, detail=str(e))
     return db_client
@@ -54,6 +58,7 @@ Create a new client by providing only the NIP number.
 """)
 async def create_client_by_nip(
     request_data: ClientNIPRequest,
+    db: Session = Depends(get_db),
     current_user: User = Depends(get_current_active_user),
 ):
     nip = request_data.nip.replace("-", "").strip()
@@ -79,14 +84,16 @@ async def create_client_by_nip(
         name=api_data.get("name", ""),
         email=None,  # fill later
         phone=None,  # fill later
-        accountNumber=api_data.get("accountNumbers", [])[0],
+        accountNumber=api_data.get("accountNumbers", [None])[0], # accountNumber can be empty list
         address=api_data.get("residenceAddress", ""),
         nip=api_data.get("nip", nip)
     )
 
     try:
-        db_client = add_client(client_create, owner_id=current_user.id)
-    except ValueError as e:
+        db_client = add_client(db, client_create, owner_id=current_user.id)
+    except HTTPException:
+        raise 
+    except Exception as e:
         raise HTTPException(status_code=409, detail=str(e))
 
     return db_client
